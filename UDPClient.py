43be78813bb48fbdc1869dec96f4992105ba1d84
tayopi <tayopi8@gmail.com>
2014-01-23 09:54:31 -0700

    Updated both application for the UDP example.

--- UDPClient.py
+++ UDPClient.py
@@ -1,1 +1,14 @@
 # Python UPD Socket Client Application
+
+from socket import *
+
+serverName = 'localhost'
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+message = input("Input lowercase sentence:")
+clientSocket.sendto(message,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage)
+clientSocket.close()
