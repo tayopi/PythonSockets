e631a5d605f75e73b6f2e221d585a2a9e2132f50
tayopi <tayopi8@gmail.com>
2014-01-23 16:47:16 -0700

    Implemented the TCP versions of the client and server applications.

--- UDPServer.py
+++ UDPServer.py
@@ -6,7 +6,7 @@ serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 
-print("The server is ready to receive")
+print "The server is ready to receive"
 
 while 1:
 	message, clientAddress = serverSocket.recvfrom(2048)
